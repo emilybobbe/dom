f30c79172cf675186e1045c6dba36f7b23dbe0b8
Emily Bobbe and Sophia Fujiki <sophia@>
2019-02-06 19:47:00 -0800

    Added image, unique messages and additional button

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,21 @@
 $(document).ready(function() {
   $("button#cat").click(function() {
-    $("ul").prepend("<li>meow</li>");
+    $("ul#cat2").append("<li class='disappear-cat'>meow meow</li>");
+    $("ul#cat2").append("<img src='img/cat.jpg'>");
+    $("ul#dog2").append("<li>How dare you!</li>");
   });
 
   $("button#dog").click(function() {
-    $("ul").prepend("<li>ruff</li>");
+    $("ul#cat2").append("<li>RUDE</li>");
+    $("ul#dog2").append("<li>ruff ruff</li>");
   });
+
+  // $(".clickable2").click(function() {
+  $("#initially-showing2").toggle();
+  // $("#initially-hidden1").toggle();
+  // });
+
+  $("#disappear").click(function() {
+    $(".disappear-cat").toggle();
+  })
 });
